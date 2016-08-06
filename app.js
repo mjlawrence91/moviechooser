fd36f2984ef9b47c10aa14730ba8be25beb377c4
Matt Lawrence <ninja570@hotmail.com>
2016-08-06 17:37:48 +0100

    Major refactor of app code
    Protected global scope
    Removed nearly all dependencies on jQuery and Underscore.js
    Render suggestors as templates
    Recreated selectText jQuery plugin as native JS function

--- app.js
+++ app.js
@@ -1,55 +1,60 @@
+// @flow
 const App = (function ($, _) {
-  const selectContainer = $('#selection')
-  const listHtml = $('#list').html()
-  const titleHtml = $('#title').html()
+  'use strict'
+
+  const WHOS = {
+    'Alex': 'info',
+    'Matt': 'primary',
+    'Rob': 'danger'
+  }
+
+  const selectContainer = document.querySelector('#selection')
+  const listHtml = document.querySelector('#list').innerHTML
+  const titleHtml = document.querySelector('#title').innerHTML
+  const whosHtml = document.querySelector('#whos').innerHTML
 
   function init () {
-    // Add movie handler
-    $('form').on('submit', addMovie)
+    _firstRender()
+    _addHandlers()
+  }
 
-    // Load movies and title, then render
-    const movies = fetchMovies()
-    render(movies)
+  function render (movies, selection) {
+    const selected = selection || 'Click Choose to select random idea...'
+    const renderSelectView = _.template(listHtml)
+    const html = renderSelectView({selected, movies, colours: WHOS})
+    selectContainer.innerHTML = html
 
-    // Load stored title if one present
-    renderTitle()
+    if (movies.length) {
+      document.querySelector('#choose').addEventListener('click', chooseRandom)
 
-    // Make the title editable when double-clicked
-    $('.title').on('dblclick', function (e) {
-      $(e.target).attr('contenteditable', true).focus()
+      const removes = document.querySelectorAll('a.remove')
+      removes.forEach(remove => remove.addEventListener('click', removeMovie))
+    }
+  }
+
+  function renderTitle () {
+    const title = localStorage.getItem('title') || 'Movie Chooser'
+    const renderTitleView = _.template(titleHtml)
+    const newTitle = renderTitleView({title})
+
+    const titleElement = document.createElement('div')
+    titleElement.innerHTML = newTitle
+
+    const container = document.querySelector('.container')
+    container.insertBefore(titleElement, container.firstChild)
+  }
+
+  function renderWhos () {
+    const renderWhosView = _.template(whosHtml)
+    const whosSelect = document.querySelector('select')
+    let options = whosSelect.innerHTML
+
+    Object.keys(WHOS).forEach(who => {
+      let whoElement = renderWhosView({who})
+      options += whoElement
     })
 
-    // Save the new title when the title loses focus
-    $('.title').blur(function (e) {
-      const newTitle = $(e.target).text()
-      const storedTitle = localStorage.getItem('title') || 'Movie Chooser'
-
-      if (newTitle) {
-        if (newTitle !== storedTitle) {
-          localStorage.setItem('title', newTitle)
-        }
-      } else {
-        $(e.target).text(storedTitle)
-      }
-
-      $(e.target).attr('contenteditable', false)
-    })
-
-    $('.title').on('doubletap', function (e) {
-      $(e.target).trigger('dblclick')
-      $(e.target).selectText()
-    })
-
-    // If title is editable and Enter is pressed, save title
-    $(document).keypress(function (e) {
-      if (e.keyCode === 13) { // Enter
-        e.preventDefault()
-
-        if ($('.title').is(':focus')) {
-          $('.title').blur()
-        }
-      }
-    })
+    whosSelect.innerHTML = options
   }
 
   function fetchMovies () {
@@ -59,7 +64,7 @@ const App = (function ($, _) {
 
   function savetoStore (newMovie) {
     const movieList = fetchMovies()
-    const isUnique = _.isEmpty(movieList.filter(movie => movie.name === newMovie.name))
+    const isUnique = movieList.filter(movie => movie.name === newMovie.name).length === 0
 
     if (isUnique) {
       movieList.push(newMovie)
@@ -69,79 +74,186 @@ const App = (function ($, _) {
     }
   }
 
-  function render (movies, selection) {
-    selection = selection || 'Click Choose to select random idea...'
-    const colours = {
-      'Alex': 'info',
-      'Matt': 'primary',
-      'Rob': 'danger'
-    }
+  function addMovie (evt) {
+    if (evt) evt.preventDefault()
 
-    const renderSelectView = _.template(listHtml)
-    const html = renderSelectView({selection, movies, colours})
-    selectContainer.html(html)
-
-    if (movies.length) {
-      $('#choose').click(chooseRandom)
-      $('a.remove').click(removeMovie)
-    }
-  }
-
-  function renderTitle () {
-    const title = localStorage.getItem('title') || 'Movie Chooser'
-    const renderTitleView = _.template(titleHtml)
-    const newTitle = renderTitleView({title})
-    $(newTitle).prependTo('.container')
-  }
-
-  function addMovie (e) {
-    if (e) e.preventDefault()
-
-    const formData = $(e.target).serializeArray()
-    const movieName = formData[0].value
-    const who = formData[1].value
+    const formData = new FormData(evt.target)
+    const movieName = formData.get('movie')
+    const who = formData.get('who')
 
     if (movieName && who) {
       const newModel = {name: movieName, who}
       savetoStore(newModel)
 
-      $('input[name=movie]').val('')
       render(fetchMovies())
+      _clearFormValues()
     }
   }
 
-  function chooseRandom (e) {
-    if (e) e.preventDefault()
+  function chooseRandom (evt) {
+    if (evt) evt.preventDefault()
 
     const movies = fetchMovies()
-
     const rand = Math.floor(Math.random() * movies.length)
     const {name} = movies[rand]
 
     render(movies, name)
   }
 
-  function removeMovie (e) {
-    if (e) e.preventDefault()
+  function removeMovie (evt) {
+    if (evt) evt.preventDefault()
 
-    const movieToDelete = $(e.target).parent().prev().html()
-    removeFromStore({name: movieToDelete})
+    const movieToDelete = evt.target.parentNode.getAttribute('data-remove')
+    removeFromStore(movieToDelete)
     render(fetchMovies())
   }
 
   function removeFromStore (movieToDelete) {
     const movies = fetchMovies()
-    const moviesWithoutRemoved = _(movies).reject(movie => movie.name === movieToDelete.name)
-
+    const moviesWithoutRemoved = movies.filter(movie => movie.name !== movieToDelete)
     const moviesString = JSON.stringify(moviesWithoutRemoved)
     localStorage.setItem('movies', moviesString)
   }
+
+  function _firstRender () {
+    // Load movies and title, then render
+    const movies = fetchMovies()
+    render(movies)
+
+    // Load stored title if one present
+    renderTitle()
+
+    // Load options for who suggested a movie
+    renderWhos()
+  }
+
+  function _addHandlers () {
+    const form = document.querySelector('form')
+    const title = document.querySelector('.title')
+
+    // Add handler to submit form
+    form.addEventListener('submit', addMovie)
+
+    // Add handler for editing title
+    title.addEventListener('dblclick', evt => {
+      // Make title editable and select all text
+      evt.target.setAttribute('contenteditable', 'true')
+      evt.target.focus()
+      _selectText(evt.target)
+
+      // Add handlers for Enter key to save title
+      document.addEventListener('keypress', _saveTitleOnEnter)
+
+      // Add handler to save title when you click/tap off title
+      title.addEventListener('blur', _saveTitleOnBlur)
+    })
+
+    // Add doubletap handler for mobile
+    $(title).on('doubletap', evt => {
+      evt.preventDefault()
+      evt.target.dispatchEvent(new MouseEvent('dblclick'))
+    })
+  }
+
+  function _saveTitleOnEnter (evt) {
+    if (evt.keyCode === 13) { // Enter
+      evt.preventDefault()
+
+      const focussed = document.querySelector(':focus')
+
+      if (focussed === evt.target) {
+        evt.target.blur()
+      }
+    }
+  }
+
+  function _saveTitleOnBlur (evt) {
+    const newTitle = evt.target.innerText
+    const storedTitle = localStorage.getItem('title') || 'Movie Chooser'
+
+    if (newTitle) {
+      if (newTitle !== storedTitle) {
+        localStorage.setItem('title', newTitle)
+      }
+    } else {
+      evt.target.innerText = storedTitle
+    }
+
+    evt.target.removeAttribute('contenteditable')
+
+    // Remove save title handlers
+    document.removeEventListener('keypress', _saveTitleOnEnter)
+    document.querySelector('.title').removeEventListener('blur', _saveTitleOnBlur)
+  }
+
+  function _clearFormValues () {
+    document.querySelector('input[name=movie]').value = ''
+    document.querySelector('select[name=who]').value = ''
+  }
+
+  // Adapted StackOverflow answer from user Tom Oakley:
+  // http://stackoverflow.com/a/12244703
+  function _selectText (element) {
+    let range = null
+    if (document.body.createTextRange) {
+      range = document.body.createTextRange()
+      range.moveToElementText(element)
+      range.select()
+    } else if (window.getSelection) {
+      const selection = window.getSelection()
+      range = document.createRange()
+      range.selectNodeContents(element)
+      selection.removeAllRanges()
+      selection.addRange(range)
+    }
+  }
+
+  /*eslint-disable */
+
+  /*
+   * jQuery Double Tap
+   * Developer: Sergey Margaritov (sergey@margaritov.net)
+   * Date: 22.10.2013
+   * Based on jquery documentation http://learn.jquery.com/events/event-extensions/
+   */
+  ;(function($){
+
+    $.event.special.doubletap = {
+      bindType: 'touchend',
+      delegateType: 'touchend',
+
+      handle: function(event) {
+        var handleObj   = event.handleObj,
+            targetData  = jQuery.data(event.target),
+            now         = new Date().getTime(),
+            delta       = targetData.lastTouch ? now - targetData.lastTouch : 0,
+            delay       = delay == null ? 300 : delay;
+
+        if (delta < delay && delta > 30) {
+          targetData.lastTouch = null;
+          event.type = handleObj.origType;
+          ['clientX', 'clientY', 'pageX', 'pageY'].forEach(function(property) {
+            event[property] = event.originalEvent.changedTouches[0][property];
+          })
+
+          // let jQuery handle the triggering of "doubletap" event handlers
+          handleObj.handler.apply(this, arguments);
+        } else {
+          targetData.lastTouch = now;
+        }
+      }
+    };
+
+  })($);
+
+  /*eslint-enable */
 
   return {
     init,
     fetchMovies,
     render,
     renderTitle,
+    renderWhos,
     savetoStore,
     removeFromStore,
     chooseRandom
@@ -149,66 +261,3 @@ const App = (function ($, _) {
 })(window.$, window._)
 
 window.$(document).ready(App.init)
-
-/*eslint-disable */
-
-// From Tom Oakley on StackOverflow: http://stackoverflow.com/a/12244703
-$.fn.selectText = function ()
-{
-  var doc = document
-  var element = this[0]
-
-  if (doc.body.createTextRange)
-  {
-    var range = document.body.createTextRange()
-    range.moveToElementText(element)
-    range.select()
-  }
-  else if (window.getSelection)
-  {
-    var selection = window.getSelection()
-    var range = document.createRange()
-    range.selectNodeContents(element)
-    selection.removeAllRanges()
-    selection.addRange(range)
-  }
-}
-
-/*
- * jQuery Double Tap
- * Developer: Sergey Margaritov (sergey@margaritov.net)
- * Date: 22.10.2013
- * Based on jquery documentation http://learn.jquery.com/events/event-extensions/
- */
-
-(function($){
-
-  $.event.special.doubletap = {
-    bindType: 'touchend',
-    delegateType: 'touchend',
-
-    handle: function(event) {
-      var handleObj   = event.handleObj,
-          targetData  = jQuery.data(event.target),
-          now         = new Date().getTime(),
-          delta       = targetData.lastTouch ? now - targetData.lastTouch : 0,
-          delay       = delay == null ? 300 : delay;
-
-      if (delta < delay && delta > 30) {
-        targetData.lastTouch = null;
-        event.type = handleObj.origType;
-        ['clientX', 'clientY', 'pageX', 'pageY'].forEach(function(property) {
-          event[property] = event.originalEvent.changedTouches[0][property];
-        })
-
-        // let jQuery handle the triggering of "doubletap" event handlers
-        handleObj.handler.apply(this, arguments);
-      } else {
-        targetData.lastTouch = now;
-      }
-    }
-  };
-
-})(jQuery);
-
-/*eslint-enable */
