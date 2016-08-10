b6c98c23359bbe9869e767d5d9c3ace29e3dfa80
Matt Lawrence <ninja570@hotmail.com>
2016-08-10 22:35:13 +0100

    Convert main app code to ES6 class

--- app.js
+++ app.js
@@ -1,40 +1,48 @@
-// @flow
-const App = (function ($, _) {
-  'use strict'
-
-  const WHOS = {
-    'Alex': 'info',
-    'Matt': 'primary',
-    'Rob': 'danger'
-  }
-
-  const selectContainer = document.querySelector('#selection')
-  const listHtml = document.querySelector('#list').innerHTML
-  const titleHtml = document.querySelector('#title').innerHTML
-  const whosHtml = document.querySelector('#whos').innerHTML
-
-  function init () {
-    _firstRender()
-    _addHandlers()
-  }
-
-  function render (movies, selection) {
-    const selected = selection || 'Click Choose to select random idea...'
-    const renderSelectView = _.template(listHtml)
-    const html = renderSelectView({selected, movies, colours: WHOS})
-    selectContainer.innerHTML = html
-
-    if (movies.length) {
-      document.querySelector('#choose').addEventListener('click', chooseRandom)
-
-      const removes = document.querySelectorAll('a.remove')
-      removes.forEach(remove => remove.addEventListener('click', removeMovie))
+class App {
+  static get WHOS () {
+    return {
+      'Alex': 'info',
+      'Matt': 'primary',
+      'Rob': 'danger'
     }
   }
 
-  function renderTitle () {
+  static get ENTER_KEYCODE () {
+    return 13
+  }
+
+  constructor () {
+    this._selectContainer = document.querySelector('#selection')
+    this._listHtml = document.querySelector('#list').innerHTML
+    this._titleHtml = document.querySelector('#title').innerHTML
+    this._whosHtml = document.querySelector('#whos').innerHTML
+
+    this._createBindings()
+  }
+
+  init () {
+    this._firstRender()
+    this._addHandlers()
+  }
+
+  render (movies, selection) {
+    const selected = selection || 'Click Choose to select random idea...'
+    const renderSelectView = window._.template(this._listHtml)
+    const html = renderSelectView({selected, movies, colours: App.WHOS})
+    this._selectContainer.innerHTML = html
+
+    if (movies.length) {
+      document.querySelector('#choose').addEventListener('click', this.chooseRandom)
+
+      const removes = document.querySelectorAll('a.remove')
+      const removesArray = Array.from(removes)
+      removesArray.forEach(remove => remove.addEventListener('click', this.removeMovie))
+    }
+  }
+
+  renderTitle () {
     const title = localStorage.getItem('title') || 'Movie Chooser'
-    const renderTitleView = _.template(titleHtml)
+    const renderTitleView = window._.template(this._titleHtml)
     const newTitle = renderTitleView({title})
 
     const titleElement = document.createElement('div')
@@ -44,12 +52,12 @@ const App = (function ($, _) {
     container.insertBefore(titleElement, container.firstChild)
   }
 
-  function renderWhos () {
-    const renderWhosView = _.template(whosHtml)
+  renderWhos () {
+    const renderWhosView = window._.template(this._whosHtml)
     const whosSelect = document.querySelector('select')
     let options = whosSelect.innerHTML
 
-    Object.keys(WHOS).forEach(who => {
+    Object.keys(App.WHOS).forEach(who => {
       let whoElement = renderWhosView({who})
       options += whoElement
     })
@@ -57,13 +65,13 @@ const App = (function ($, _) {
     whosSelect.innerHTML = options
   }
 
-  function fetchMovies () {
+  fetchMovies () {
     const moviesString = localStorage.getItem('movies')
     return JSON.parse(moviesString) || []
   }
 
-  function savetoStore (newMovie) {
-    const movieList = fetchMovies()
+  savetoStore (newMovie) {
+    const movieList = this.fetchMovies()
     const isUnique = movieList.filter(movie => movie.name === newMovie.name).length === 0
 
     if (isUnique) {
@@ -74,7 +82,7 @@ const App = (function ($, _) {
     }
   }
 
-  function addMovie (evt) {
+  addMovie (evt) {
     if (evt) evt.preventDefault()
 
     const formData = new FormData(evt.target)
@@ -83,83 +91,104 @@ const App = (function ($, _) {
 
     if (movieName && who) {
       const newModel = {name: movieName, who}
-      savetoStore(newModel)
+      this.savetoStore(newModel)
 
-      render(fetchMovies())
-      _clearFormValues()
+      this.render(this.fetchMovies())
+      this._clearFormValues()
 
       // Re-focus name field for new entry
       document.querySelector('input[name=movie]').focus()
     }
   }
 
-  function chooseRandom (evt) {
+  chooseRandom (evt) {
     if (evt) evt.preventDefault()
 
-    const movies = fetchMovies()
+    const movies = this.fetchMovies()
     const rand = Math.floor(Math.random() * movies.length)
     const {name} = movies[rand]
 
-    render(movies, name)
+    this.render(movies, name)
   }
 
-  function removeMovie (evt) {
+  removeMovie (evt) {
     if (evt) evt.preventDefault()
 
-    const movieToDelete = evt.target.parentNode.getAttribute('data-remove')
-    removeFromStore(movieToDelete)
-    render(fetchMovies())
+    const movieLink = evt.target.parentNode
+    movieLink.removeEventListener('click', this.removeMovie)
+
+    const movieToDelete = movieLink.getAttribute('data-remove')
+    this.removeFromStore(movieToDelete)
+    this.render(this.fetchMovies())
   }
 
-  function removeFromStore (movieToDelete) {
-    const movies = fetchMovies()
+  removeFromStore (movieToDelete) {
+    const movies = this.fetchMovies()
     const moviesWithoutRemoved = movies.filter(movie => movie.name !== movieToDelete)
     const moviesString = JSON.stringify(moviesWithoutRemoved)
     localStorage.setItem('movies', moviesString)
   }
 
-  function _firstRender () {
-    // Load movies and title, then render
-    const movies = fetchMovies()
-    render(movies)
-
-    // Load stored title if one present
-    renderTitle()
-
-    // Load options for who suggested a movie
-    renderWhos()
+  _createBindings () {
+    this.init = this.init.bind(this)
+    this.render = this.render.bind(this)
+    this.renderTitle = this.renderTitle.bind(this)
+    this.renderWhos = this.renderWhos.bind(this)
+    this.fetchMovies = this.fetchMovies.bind(this)
+    this.savetoStore = this.savetoStore.bind(this)
+    this.addMovie = this.addMovie.bind(this)
+    this.chooseRandom = this.chooseRandom.bind(this)
+    this.removeMovie = this.removeMovie.bind(this)
+    this.removeFromStore = this.removeFromStore.bind(this)
+    this._firstRender = this._firstRender.bind(this)
+    this._addHandlers = this._addHandlers.bind(this)
+    this._saveTitleOnEnter = this._saveTitleOnEnter.bind(this)
+    this._saveTitleOnBlur = this._saveTitleOnBlur.bind(this)
+    this._clearFormValues = this._clearFormValues.bind(this)
   }
 
-  function _addHandlers () {
+  _firstRender () {
+    // Load movies and title, then render
+    const movies = this.fetchMovies()
+    this.render(movies)
+
+    // Load stored title if one present
+    this.renderTitle()
+
+    // Load options for who suggested a movie
+    this.renderWhos()
+  }
+
+  _addHandlers () {
     const form = document.querySelector('form')
     const title = document.querySelector('.title')
 
     // Add handler to submit form
-    form.addEventListener('submit', addMovie)
+    form.addEventListener('submit', this.addMovie)
 
     // Add handler for editing title
     title.addEventListener('dblclick', evt => {
       // Make title editable and select all text
       evt.target.setAttribute('contenteditable', 'true')
       evt.target.focus()
-      _selectText(evt.target)
+      this._selectText(evt.target)
 
       // Add handlers for Enter key to save title
-      document.addEventListener('keypress', _saveTitleOnEnter)
+      document.addEventListener('keypress', this._saveTitleOnEnter)
 
       // Add handler to save title when you click/tap off title
-      title.addEventListener('blur', _saveTitleOnBlur)
+      title.addEventListener('blur', this._saveTitleOnBlur)
     })
 
     // Add doubletap handler for mobile
-    $(title).on('doubletap', evt => {
+    window.$(title).on('doubletap', evt => {
       evt.preventDefault()
       evt.target.dispatchEvent(new MouseEvent('dblclick'))
     })
   }
 
-  function _saveTitleOnEnter (evt) {
-    if (evt.keyCode === 13) { // Enter
+  _saveTitleOnEnter (evt) {
+    if (evt.keyCode === App.ENTER_KEYCODE) {
       evt.preventDefault()
 
       const focussed = document.querySelector(':focus')
@@ -170,7 +199,7 @@ const App = (function ($, _) {
     }
   }
 
-  function _saveTitleOnBlur (evt) {
+  _saveTitleOnBlur (evt) {
     const newTitle = evt.target.innerText
     const storedTitle = localStorage.getItem('title') || 'Movie Chooser'
 
@@ -185,82 +214,55 @@ const App = (function ($, _) {
     evt.target.removeAttribute('contenteditable')
 
     // Remove save title handlers
-    document.removeEventListener('keypress', _saveTitleOnEnter)
-    document.querySelector('.title').removeEventListener('blur', _saveTitleOnBlur)
+    document.removeEventListener('keypress', this._saveTitleOnEnter)
+    document.querySelector('.title').removeEventListener('blur', this._saveTitleOnBlur)
   }
 
-  function _clearFormValues () {
+  _clearFormValues () {
     document.querySelector('input[name=movie]').value = ''
     document.querySelector('select[name=who]').value = ''
   }
+}
 
-  // Adapted StackOverflow answer from user Tom Oakley:
-  // http://stackoverflow.com/a/12244703
-  function _selectText (element) {
-    let range = null
-    if (document.body.createTextRange) {
-      range = document.body.createTextRange()
-      range.moveToElementText(element)
-      range.select()
-    } else if (window.getSelection) {
-      const selection = window.getSelection()
-      range = document.createRange()
-      range.selectNodeContents(element)
-      selection.removeAllRanges()
-      selection.addRange(range)
-    }
-  }
+/*eslint-disable */
 
-  /*eslint-disable */
+/*
+ * jQuery Double Tap
+ * Developer: Sergey Margaritov (sergey@margaritov.net)
+ * Date: 22.10.2013
+ * Based on jquery documentation http://learn.jquery.com/events/event-extensions/
+ */
+;(function($){
 
-  /*
-   * jQuery Double Tap
-   * Developer: Sergey Margaritov (sergey@margaritov.net)
-   * Date: 22.10.2013
-   * Based on jquery documentation http://learn.jquery.com/events/event-extensions/
-   */
-  ;(function($){
+  $.event.special.doubletap = {
+    bindType: 'touchend',
+    delegateType: 'touchend',
 
-    $.event.special.doubletap = {
-      bindType: 'touchend',
-      delegateType: 'touchend',
+    handle: function(event) {
+      var handleObj   = event.handleObj,
+          targetData  = jQuery.data(event.target),
+          now         = new Date().getTime(),
+          delta       = targetData.lastTouch ? now - targetData.lastTouch : 0,
+          delay       = delay == null ? 300 : delay;
 
-      handle: function(event) {
-        var handleObj   = event.handleObj,
-            targetData  = jQuery.data(event.target),
-            now         = new Date().getTime(),
-            delta       = targetData.lastTouch ? now - targetData.lastTouch : 0,
-            delay       = delay == null ? 300 : delay;
+      if (delta < delay && delta > 30) {
+        targetData.lastTouch = null;
+        event.type = handleObj.origType;
+        ['clientX', 'clientY', 'pageX', 'pageY'].forEach(function(property) {
+          event[property] = event.originalEvent.changedTouches[0][property];
+        })
 
-        if (delta < delay && delta > 30) {
-          targetData.lastTouch = null;
-          event.type = handleObj.origType;
-          ['clientX', 'clientY', 'pageX', 'pageY'].forEach(function(property) {
-            event[property] = event.originalEvent.changedTouches[0][property];
-          })
-
-          // let jQuery handle the triggering of "doubletap" event handlers
-          handleObj.handler.apply(this, arguments);
-        } else {
-          targetData.lastTouch = now;
-        }
+        // let jQuery handle the triggering of "doubletap" event handlers
+        handleObj.handler.apply(this, arguments);
+      } else {
+        targetData.lastTouch = now;
       }
-    };
+    }
+  };
 
-  })($);
+})($);
 
-  /*eslint-enable */
+/*eslint-enable */
 
-  return {
-    init,
-    fetchMovies,
-    render,
-    renderTitle,
-    renderWhos,
-    savetoStore,
-    removeFromStore,
-    chooseRandom
-  }
-})(window.$, window._)
-
-window.$(document).ready(App.init)
+const app = new App()
+window.addEventListener('load', app.init)
