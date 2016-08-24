8ba55c300884e34065fd3a68d5568dfea9a2d6d9
Matt Lawrence <ninja570@hotmail.com>
2016-08-24 21:43:56 +0100

    Add Node.js/MongoDB backend

---
+++ server/app.js
@@ -0,0 +1,23 @@
+import path from 'path'
+import express from 'express'
+import bodyParser from 'body-parser'
+import serveStatic from 'serve-static'
+import errorHandler from './middleware/errorHandler'
+import routes from './routes'
+
+const staticPath = (process.env.NODE_ENV !== 'dev')
+  ? path.resolve(__dirname, '../dist')
+  : path.resolve(__dirname, '../')
+
+const app = express()
+app.set('x-powered-by', false)
+app.use(bodyParser.json())
+app.use(errorHandler)
+app.use(serveStatic(staticPath))
+
+app.get('/debug', (req, res) => res.status(200).send('Hello World'))
+app.get('/api/movies/:id?', routes.read)
+app.post('/api/movies', routes.create)
+app.delete('/api/movies/:id', routes.delete)
+
+export default app
