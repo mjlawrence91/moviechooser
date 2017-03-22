9b852f91a8108cfc595990448176a953989159dc
Matt Lawrence <ninja570@hotmail.com>
2017-03-22 10:41:59 +0000

    Minify main and inline styles

---
+++ build/minifyStyles.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const pify = require('pify')
+const CleanCSS = require('clean-css')
+
+const readFile = pify(fs.readFile)
+const writeFile = pify(fs.writeFile)
+
+const inPath = path.join(__dirname, '..', 'src', 'client', 'styles')
+const outPath = path.join(__dirname, '..', 'dist')
+
+const files = [
+  {
+    in: path.join(inPath, 'inline-styles.css'),
+    out: path.join(outPath, 'client', 'styles', 'inline-styles.css')
+  },
+  {
+    in: path.join(inPath, 'main-styles.css'),
+    out: path.join(outPath, 'client', 'styles', 'styles.css')
+  }
+]
+
+files.forEach(async file => {
+  const buffer = await readFile(file.in)
+  const styles = buffer.toString('utf-8')
+
+  const output = new CleanCSS().minify(styles)
+  await writeFile(file.out, output.styles, 'utf-8')
+})
