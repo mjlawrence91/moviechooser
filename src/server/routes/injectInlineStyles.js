9b3b7df038f7a5861f965fed780a701ded696629
Matt Lawrence <ninja570@hotmail.com>
2017-03-22 10:42:36 +0000

    Inject inline styles into HTML

---
+++ src/server/routes/injectInlineStyles.js
@@ -0,0 +1,24 @@
+import fs from 'fs'
+import path from 'path'
+import pify from 'pify'
+import dot from 'dot'
+
+const staticPath = path.resolve(__dirname, '../../client')
+const readFile = pify(fs.readFile)
+
+async function injectInlineStyles (req, res) {
+  // Read inline styles
+  const stylesBuffer = await readFile(`${staticPath}/styles/inline-styles.css`)
+  const styles = stylesBuffer.toString('utf-8')
+
+  // Read index.html
+  const htmlBuffer = await readFile(`${staticPath}/index.html`)
+  const html = htmlBuffer.toString('utf-8')
+
+  // Inject inline styles into index.html and serve
+  const template = dot.template(html)
+  const injectedHtml = template({styles})
+  res.send(injectedHtml)
+}
+
+export default injectInlineStyles
