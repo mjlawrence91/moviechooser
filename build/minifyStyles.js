3594eb8cbbd70d2273bfa5997d4793b4942675ac
Matt Lawrence <mjlawrence91@gmail.com>
2022-09-02 21:50:19 +0100

    Update NPM dependencies as part of migration to Render Remove promisify and use built-in fs/promises API Use Firebase compat modules after upgrade

--- build/minifyStyles.js
+++ build/minifyStyles.js
@@ -1,17 +1,12 @@
-const fs = require('fs')
+const fs = require('fs/promises')
 const path = require('path')
-const { promisify } = require('util')
 const CleanCSS = require('clean-css')
-
-const readFile = promisify(fs.readFile)
-const writeFile = promisify(fs.writeFile)
-const mkdir = promisify(fs.mkdir)
 
 export default async function minifyStyles () {
   const inPath = path.join(__dirname, 'src/client/styles')
   const outPath = path.join(__dirname, 'dist')
 
-  await mkdir(path.join(outPath, 'client/styles'), { recursive: true })
+  await fs.mkdir(path.join(outPath, 'client/styles'), { recursive: true })
 
   const files = [
     {
@@ -25,13 +20,13 @@ export default async function minifyStyles () {
   ]
 
   files.forEach(async file => {
-    const styles = await readFile(file.in, { encoding: 'utf-8' }).catch(error =>
+    const styles = await fs.readFile(file.in, { encoding: 'utf-8' }).catch(error =>
       console.error(`Error reading file: ${error}`)
     )
 
     const output = new CleanCSS().minify(styles)
 
-    await writeFile(file.out, output.styles, 'utf-8').catch(error =>
+    await fs.writeFile(file.out, output.styles, 'utf-8').catch(error =>
       console.error(`Error writing file: ${error}`)
     )
   })
--- src/server/routes/loadServiceWorker.js
+++ src/server/routes/loadServiceWorker.js
@@ -1,12 +1,10 @@
-import fs from 'fs'
+import fs from 'fs/promises'
 import path from 'path'
-import { promisify } from 'util'
 
 const staticPath = path.resolve(__dirname, '../../client')
-const readFile = promisify(fs.readFile)
 
 export default async function loadServiceWorker (req, res) {
-  const serviceWorker = await readFile(`${staticPath}/sw.js`).then(buffer =>
+  const serviceWorker = await fs.readFile(`${staticPath}/sw.js`).then(buffer =>
     buffer.toString('utf-8')
   )
 
